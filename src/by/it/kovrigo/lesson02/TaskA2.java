28c88a6f143d100c18c65665b450cc70a4eff7f5
vnlanevich <vnlanevich@gmail.com>
2019-02-22 15:39:26 +0300

    Lesson2a1

--- src/by/it/kovrigo/lesson02/TaskA2.java
+++ src/by/it/kovrigo/lesson02/TaskA2.java
@@ -11,7 +11,7 @@ package by.it.kovrigo.lesson02;
 class TaskA2 {
     public static void main(String[] args) {
         System.out.println("Я начинаю изучать Java!");
-        for (i=0;i)
+      
     }
 }
 
